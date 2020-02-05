2247dfbca4c3d35ad946444cf62dac02e8fe28ba
David Guimaraes <david.ce@live.com>
2020-02-05 05:02:18 -0400

    04. Configurando conexão com o bd Postgres e criando script DDL para criar tabelas no banco através de migração

---
+++ backend-microservice/src/main/resources/db/migration/V01__criar_licitacoes_e_propostas.sql
@@ -0,0 +1,38 @@
+CREATE TYPE tipo_classificacao AS ENUM('MENOR PRECO', 'NOTA PRECO');
+
+CREATE SEQUENCE public.licitacao_licitacao_id_seq;
+
+CREATE TABLE public.licitacao (
+                licitacao_id BIGINT NOT NULL DEFAULT nextval('public.licitacao_licitacao_id_seq'),
+                descricao VARCHAR(100) NOT NULL,
+                tipo_classificacao tipo_classificacao NOT NULL,
+                data_cadastro DATE NOT NULL,
+                data_ultima_edicao DATE,
+                CONSTRAINT licitacao_pk PRIMARY KEY (licitacao_id)
+);
+
+
+ALTER SEQUENCE public.licitacao_licitacao_id_seq OWNED BY public.licitacao.licitacao_id;
+
+CREATE SEQUENCE public.proposta_proposta_id_seq;
+
+CREATE TABLE public.proposta (
+                proposta_id BIGINT NOT NULL DEFAULT nextval('public.proposta_proposta_id_seq'),
+                nota NUMERIC NOT NULL,
+                preco NUMERIC NOT NULL,
+                data_cadastro DATE NOT NULL,
+                classificacao INTEGER NOT NULL,
+                licitacao_id BIGINT NOT NULL,
+                data_ultima_edicao DATE,
+                CONSTRAINT proposta_pk PRIMARY KEY (proposta_id)
+);
+
+
+ALTER SEQUENCE public.proposta_proposta_id_seq OWNED BY public.proposta.proposta_id;
+
+ALTER TABLE public.proposta ADD CONSTRAINT licitacao_proposta_fk
+FOREIGN KEY (licitacao_id)
+REFERENCES public.licitacao (licitacao_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
