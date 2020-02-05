954733502e315ffc0572532b7f904cade0524476
David Guimaraes <david.ce@live.com>
2020-02-05 04:00:53 -0400

    01. Criados pacote para o backend e o projeto Spring em si

---
+++ backend-microservice/src/test/java/com/az/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.az.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
