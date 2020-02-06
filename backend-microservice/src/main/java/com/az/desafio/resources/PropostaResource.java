5e0b5dec3310aac8053d6f321ee78004823dffaf
David Guimaraes <david.ce@live.com>
2020-02-06 14:02:12 -0400

    17. Endpoints básicos de CRUD da entidade Proposta implementados

--- backend-microservice/src/main/java/com/az/desafio/resources/PropostaResource.java
+++ backend-microservice/src/main/java/com/az/desafio/resources/PropostaResource.java
@@ -1,9 +1,74 @@
 package com.az.desafio.resources;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.az.desafio.models.Proposta;
+import com.az.desafio.models.dto.PropostaDto;
+import com.az.desafio.models.mappers.PropostaMapper;
+import com.az.desafio.services.PropostaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.math.BigInteger;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/rest/propostas")
 public class PropostaResource {
+
+    @Autowired
+    private PropostaService propostaService;
+
+    @Autowired
+    private PropostaMapper propostaMapper;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+//    @PreAuthorize("isAuthenticated()")
+    public Page<PropostaDto> listar(Pageable pageable) throws Exception {
+        try {
+            pageable = pageable.previousOrFirst();
+            Page<Proposta> propostas = propostaService.findAll(pageable);
+            return propostas.map(propostaMapper::entityToDto);
+        } catch (Exception e) {
+            throw new Exception("Erro ao buscar dados: " + e.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    @PreAuthorize("isAuthenticated()")
+    public PropostaDto pesquisarPeloId(@PathVariable(value = "id") BigInteger id) throws Exception {
+        Proposta proposta = propostaService.findById(id);
+        return propostaMapper.entityToDto(proposta);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PropostaDto> salvar(@Valid @RequestBody PropostaDto propostaDto){
+        Proposta proposta = propostaMapper.dtoToEntity(propostaDto);
+
+        Proposta propostaSalva = propostaService.save(proposta);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(proposta.getPropostaId()).toUri();
+
+        return ResponseEntity.created(uri).body(propostaMapper.entityToDto(propostaSalva));
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PropostaDto> editar(@Valid @RequestBody PropostaDto propostaDto) throws Exception {
+
+        Proposta proposta = propostaMapper.dtoToEntity(propostaDto);
+        Proposta propostaSalva = propostaService.update(proposta);
+
+        return ResponseEntity.ok(propostaMapper.entityToDto(propostaSalva));
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> deletar(@PathVariable("id") BigInteger id) throws Exception{
+        propostaService.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend-microservice/src/main/java/com/az/desafio/services/PropostaService.java
+++ backend-microservice/src/main/java/com/az/desafio/services/PropostaService.java
@@ -1,7 +1,63 @@
 package com.az.desafio.services;
 
+import com.az.desafio.models.Proposta;
+import com.az.desafio.repositories.PropostaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class PropostaService {
+
+    @Autowired
+    private PropostaRepository dao;
+
+    public Page<Proposta> findAll(Pageable pageable){
+        return this.dao.findAll(pageable);
+    }
+
+    public Proposta findById(BigInteger id) throws Exception{
+        Optional<Proposta> propostaSalva = this.dao.findById(id);
+        if(propostaSalva.isPresent()){
+            return propostaSalva.get();
+        }else {
+            throw new Exception("Não foi encontrada uma proposta com o id informado");
+        }
+    }
+
+    public Proposta save(Proposta proposta){
+        proposta.setDataCadastro(new Date());
+        proposta.setDataUltimaEdicao(null);
+        proposta.setClassificacao(0);
+        this.dao.save(proposta);
+        return proposta;
+    }
+
+    public Proposta update(Proposta proposta) throws Exception{
+        Optional<Proposta> propostaSalva = this.dao.findById(proposta.getPropostaId());
+        if(propostaSalva.isPresent()){
+            propostaSalva.get().setNota(proposta.getNota());
+            propostaSalva.get().setPreco(proposta.getPreco());
+            propostaSalva.get().setLicitacao(proposta.getLicitacao());
+            propostaSalva.get().setDataUltimaEdicao(new Date());
+            this.dao.save(propostaSalva.get());
+            return propostaSalva.get();
+        }else{
+            throw new Exception("Não foi encontrada uma proposta com o id informado");
+        }
+    }
+
+    public void delete(BigInteger id) throws Exception{
+        Optional<Proposta> propostaSalva = this.dao.findById(id);
+        if(propostaSalva.isPresent()){
+            this.dao.delete(propostaSalva.get());
+        }else{
+            throw new Exception("Não foi encontrada uma proposta com o id informado");
+        }
+    }
 }
