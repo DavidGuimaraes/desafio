57e94bf47b01aa20b189810f7732c60948852d71
David Guimaraes <david.ce@live.com>
2020-02-06 10:25:19 -0400

    08. Criado o segundo script de migração para povoar a tabela licitacao no banco de dados

---
+++ backend-microservice/src/main/resources/db/migration/V02__povoar_licitacoes.sql
@@ -0,0 +1,14 @@
+INSERT INTO licitacao (descricao, tipo_classificacao, data_cadastro, data_ultima_edicao)
+VALUES ('Licitação de teste', 'MENOR_PRECO'::tipo_classificacao, CURRENT_DATE, NULL);
+
+INSERT INTO licitacao (descricao, tipo_classificacao, data_cadastro, data_ultima_edicao)
+VALUES ('Licitação para arroz', 'MENOR_PRECO'::tipo_classificacao, CURRENT_DATE, NULL);
+
+INSERT INTO licitacao (descricao, tipo_classificacao, data_cadastro, data_ultima_edicao)
+VALUES ('Licitação para feijão', 'MENOR_PRECO'::tipo_classificacao, CURRENT_DATE, NULL);
+
+INSERT INTO licitacao (descricao, tipo_classificacao, data_cadastro, data_ultima_edicao)
+VALUES ('Licitação para peças de informática', 'MENOR_PRECO'::tipo_classificacao, CURRENT_DATE, NULL);
+
+INSERT INTO licitacao (descricao, tipo_classificacao, data_cadastro, data_ultima_edicao)
+VALUES ('Licitação para obra de pavimentação', 'MENOR_PRECO'::tipo_classificacao, CURRENT_DATE, NULL);
