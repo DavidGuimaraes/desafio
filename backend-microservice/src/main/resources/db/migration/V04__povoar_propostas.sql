d761667b7923f97e659f23eb1b26ffbff6e62e1b
David Guimaraes <david.ce@live.com>
2020-02-06 13:07:18 -0400

    14. Criado o quarto script de migração para povoar a tabela proposta no banco de dados

---
+++ backend-microservice/src/main/resources/db/migration/V04__povoar_propostas.sql
@@ -0,0 +1,23 @@
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (7.5, 250, CURRENT_DATE, NULL, 2, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (8.0, 200, CURRENT_DATE, NULL, 2, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (8.5, 150, CURRENT_DATE, NULL, 2, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (9.5, 100, CURRENT_DATE, NULL, 2, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (6.5, 250, CURRENT_DATE, NULL, 3, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (7.0, 200, CURRENT_DATE, NULL, 3, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (7.5, 150, CURRENT_DATE, NULL, 3, NULL);
+
+INSERT INTO proposta (nota, preco, data_cadastro, classificacao, licitacao_id, data_ultima_edicao)
+VALUES (8.5, 100, CURRENT_DATE, NULL, 3, NULL);
