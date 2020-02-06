2898d2d889f8b6c6350880cbb398fcaeba94e0b0
David Guimaraes <david.ce@live.com>
2020-02-06 16:33:33 -0400

    24. Implementada a estrutura básica das páginas de licitações e propostas com dados hard coded

--- frontend-sofea-vue/src/views/Licitacao.vue
+++ frontend-sofea-vue/src/views/Licitacao.vue
@@ -1,8 +1,35 @@
 <template>
     <div>
         <Navbar />
+        <br />
         <div class="container">
-            LICITAÇŌES VIRÃO AQUI
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <b>LICITAÇŌES</b>
+          </div>
+          <br />
+          <br />
+          <br />
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Descrição</th>
+                  <th scope="col">Tipo de Classificação</th>
+                  <th scope="col">Data de Cadastro</th>
+                  <th scope="col">Data Última Edição</th>
+                </tr>
+              </thead>
+              <tbody v-if="licitacoes.length > 0">
+                <tr v-for="(licitacao, index) in licitacoes" v-bind:key="index">
+                  <th scope="row">{{ licitacao.id }}</th>
+                  <td>{{ licitacao.descricao }}</td>
+                  <td>{{ licitacao.tipoClassificacao }}</td>
+                  <td>{{ licitacao.dataCadastro }}</td>
+                  <td>{{ licitacao.dataUltimaEdicao }}</td>
+                </tr>
+              </tbody>
+              <p v-else>Nenhum dado foi encontrado...</p>
+            </table>
         </div>
     </div>
 </template>
@@ -14,6 +41,24 @@ export default {
   name: 'app',
   components: {
     Navbar
+  },
+  data() {
+    return {
+      strings:[
+        'Mark', 
+        'Otto', 
+        'Billy', 
+        '@mdo'],
+      licitacoes: [
+          {
+            id: 1,
+            descricao: "Teste Hard Coded",
+            tipoClassificacao: "NOTA_PRECO",
+            dataCadastro: "2020-02-06",
+            dataUltimaEdicao: "-"
+          }
+        ]
+    }
   }
 }
 </script>
--- frontend-sofea-vue/src/views/Proposta.vue
+++ frontend-sofea-vue/src/views/Proposta.vue
@@ -1,8 +1,39 @@
 <template>
     <div>
         <Navbar />
+        <br />
         <div class="container">
-            PROPOSTAS VIRÃO AQUI
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+            <b>PROPOSTAS</b>
+          </div>
+          <br />
+          <br />
+          <br />
+          <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">Id</th>
+                  <th scope="col">Nota</th>
+                  <th scope="col">Preço</th>
+                  <th scope="col">Classificação</th>
+                  <th scope="col">Licitação</th>
+                  <th scope="col">Data de Cadastro</th>
+                  <th scope="col">Data Última Edição</th>
+                </tr>
+              </thead>
+              <tbody v-if="propostas.length > 0">
+                <tr v-for="(proposta, index) in propostas" v-bind:key="index">
+                  <th scope="row">{{ proposta.id }}</th>
+                  <td>{{ proposta.nota }}</td>
+                  <td>{{ proposta.preco }}</td>
+                  <td>{{ proposta.classificacao }}</td>
+                  <td>{{ proposta.licitacao.id }}</td>
+                  <td>{{ proposta.dataCadastro }}</td>
+                  <td>{{ proposta.dataUltimaEdicao }}</td>
+                </tr>
+              </tbody>
+              <p v-else>Nenhum dado foi encontrado...</p>
+            </table>
         </div>
     </div>
 </template>
@@ -14,6 +45,27 @@ export default {
   name: 'app',
   components: {
     Navbar
+  },
+  data() {
+    return {
+      propostas: [
+          {
+            id: 1,
+            nota: 8.0,
+            preco: 250,
+            classificacao: 0,
+            licitacao: {
+                id: 1,
+                descricao: "Teste Hard Coded",
+                tipoClassificacao: "NOTA_PRECO",
+                dataCadastro: "2020-02-06",
+                dataUltimaEdicao: ""
+            },
+            dataCadastro: "2020-02-06",
+            dataUltimaEdicao: "-"
+          }
+        ]
+    }
   }
 }
 </script>
