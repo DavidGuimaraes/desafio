c6d2c0f993bfaf4193914d8533293c79c76774a1
David Guimaraes <david.ce@live.com>
2020-02-06 23:37:01 -0400

    34. Alterando o Enum para deserialização com Jackson

--- backend-microservice/src/main/java/com/az/desafio/models/enums/TipoClassificacao.java
+++ backend-microservice/src/main/java/com/az/desafio/models/enums/TipoClassificacao.java
@@ -1,7 +1,12 @@
 package com.az.desafio.models.enums;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum TipoClassificacao {
+    @JsonProperty("Menor Preço")
     MENOR_PRECO("Menor Preço"),
+    @JsonProperty("Nota Preço")
     NOTA_PRECO("Nota Preço");
 
     TipoClassificacao(String s) {
