3225d036fc1e6b4e6b5e6b429656eb0c106fa7ab
David Guimaraes <david.ce@live.com>
2020-02-06 22:23:52 -0400

    33. Chamada à API para consultar todos os tipo de classificação e povoar um dropdown em NovoLicitacao

--- frontend-sofea-vue/src/services/licitacaoservice.js
+++ frontend-sofea-vue/src/services/licitacaoservice.js
@@ -3,5 +3,8 @@ import { http } from './config'
 export default {
     listar:() => {
         return http.get('licitacoes')
+    },
+    listarTiposClassificacao:() => {
+        return http.get('licitacoes/classificacoes')
     }
 }
--- frontend-sofea-vue/src/views/licitacao/NovoLicitacao.vue
+++ frontend-sofea-vue/src/views/licitacao/NovoLicitacao.vue
@@ -13,25 +13,39 @@
             <div class="row">
                 <div class="col-md-5 mb-3">
                     <label for="classificacao">Tipo de Classificação</label>
-                    <select class="custom-select d-block w-100" id="country" required>
-                        <option value="">Escolha...</option>
-                        <option>Teste</option>
+                    <select class="custom-select d-block w-100" id="country" required v-bind="tipoSelectionado">
+                        <option value="" v-for="(tipo, index) in tiposClassificacao" v-bind:key="index">
+                            {{ tipo }}
+                        </option>
                     </select>
                 </div>
             </div>
             <button type="button" class="btn btn-primary">Salvar</button>
             <router-link to="/licitacoes/" class="btn btn-danger">Cancelar</router-link>
         </div>
+        <p>{{ tipoSelectionado }}</p>
     </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar'
+import LicitacaoService from '@/services/licitacaoservice'
 
 export default {
   name: 'app',
   components: {
     Navbar
+  },
+  data() {
+      return {
+          tiposClassificacao: [],
+          tipoSelectionado: ""
+      }
+  },
+  mounted() {
+      LicitacaoService.listarTiposClassificacao().then(response => {
+      this.tiposClassificacao = response.data
+    })
   }
 }
 </script>
